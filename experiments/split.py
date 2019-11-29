909035148dde640e7eab2fc196096c0bc01abd9d
Jesse Postema <mail@jessepostema.nl>
2019-11-29 13:31:11 +0100

    added more commandline options

--- experiments/split.py
+++ experiments/split.py
@@ -4,58 +4,79 @@ import csv
 
 argumentList = sys.argv[1:]
 
-unixOptions = "hf:s:t:d:c:"
-gnuOptions = ["help", "file=", "station=", "transport=", "delay=", "cancelled="]
+unixOptions = ""
+gnuOptions = ["help", "file=", "date=", "station=", "number=", "transporter=",
+            "vehicle=", "destination=", "delay=", "cancelled="]
 
+def usageGuide():
+    print("split.py usage:")
+    print("--help:\t\tshow usage guide")
+    print("--file:\t\tset destination file\t\t\t\t\t\t(mandatory)")
+    print("--station:\tfilter on specific station\t\t\t(default: none)\t(optional)")
+    print("--number\tfilter on specific train number\t\t\t(default: none)\t(optional)")
+    print("--transporter:\tfilter on specific transport carrier\t\t(default: none)\t(optional)")
+    print("--vehicle:\tfilter on specific vehicle type\t\t\t(default: none)\t(optional)")
+    print("--destination:\tfilter on specific destination\t\t\t(default: none)\t(optional)")
+    print("--delay:\tfilter on having equal to or greater than delay\t(default: none)\t(optional)")
+    print("--cancelled:\tfilter on cancelled or not (yes/no/both)\t(default: both)\t(optional)")
 
 try:
     arguments, _ = getopt.getopt(argumentList, unixOptions, gnuOptions)
 except getopt.error as err:
     print(str(err))
+    usageGuide()
     exit()
 
 destinationFile = ""
+date = ""
 station = ""
-transport = ""
+number = ""
+transporter = ""
+vehicle = ""
+destination = ""
 delay = ""
 cancelled = ""
 
-def usageGuide():
-    print("split.py usage:")
-    print("--help (-h):\t\tshow usage guide")
-    print("--file (-f):\t\tset destination file\t\t\t\t\t\t(mandatory)")
-    print("--station (-s):\t\tfilter on specific station\t\t\t(default: none)\t(optional)")
-    print("--transport (-t):\tfilter on specific transport carrier\t\t(default: none)\t(optional)")
-    print("--delay (-d):\t\tfilter on having equal to or greater than delay\t(default: none)\t(optional)")
-    print("--cancelled (-c):\tfilter on cancelled or not (yes/no/both)\t(default: both)\t(optional)")
-
 for arg, value in arguments:
-    if arg in ('-h', "--help"):
+    if arg == '--help':
         usageGuide()
         exit()
-    elif arg in ('-f', '--file'):
+    elif arg == '--file':
         print("setting destination file to %s" % (value))
         destinationFile = value
-    elif arg in ('-s', '--station'):
+    elif arg == '--date':
+        print("filtering on date %s" % (value))
+        date = value
+    elif arg == '--station':
         print("filtering on station %s" % (value))
         station = value
-    elif arg in ('-t', '--transport'):
+    elif arg == '--number':
+        print("filtering on train number %s" % (value))
+        number = value
+    elif arg == '--transporter':
         print("filtering on transport carrier %s" % (value))
-        transport = value
-    elif arg in ('-d', '--delay'):
+        transporter = value
+    elif arg == '--vehicle':
+        print("filtering on vehicle type %s" % (value))
+        vehicle = value
+    elif arg == '--destination':
+        print("filtering on destination %s" % (value))
+        destination = value
+    elif arg == '--delay':
         print("filtering on having delay equal to or greater than %s" % (value))
         delay = value
-    elif arg in ('-c', '--cancelled'):
+    elif arg == '--cancelled':
         if (value == 'yes'):
             print("filtering on cancelled only")
             cancelled = 'yes'
         elif (value == 'no'):
             print("filtering on not cancelled only")
             cancelled = 'no'
-        else:
+        elif (value != 'both'):
             usageGuide()
             exit()
 
+# having a destinationfile is mandatory
 if (destinationFile == ""):
     usageGuide()
     exit()
