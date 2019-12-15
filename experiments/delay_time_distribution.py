c79cf7583f65784080855c5eac32194ab02be2d1
Joël Buter <jtbuter@gmail.com>
2019-12-15 12:57:31 +0100

    added delays distribution

--- experiments/delay_time_distribution.py
+++ experiments/delay_time_distribution.py
@@ -1,6 +1,9 @@
-import seaborn as sns
+# histogram showing amount of delayed stops per weekday at 8am
+import scipy.stats as st
 import matplotlib.pyplot as plt
 import csv
+import datetime
+from collections import OrderedDict
 import numpy as np
 
 columns = [
@@ -8,9 +11,9 @@ columns = [
     "traintype", "destination", "time", "delay", "canceled"
 ]
 
+# Dictionary contains dates as keys and sets of trainnumbers as values
+delayed_trains = {}
 delays = []
-
-gt = 0
 
 with open("../data/NS_delayed_trains_2019_08_01_to_2019_11_01.csv") as vertrektijden:
     reader = csv.reader(vertrektijden, delimiter=",")
@@ -18,18 +21,19 @@ with open("../data/NS_delayed_trains_2019_08_01_to_2019_11_01.csv") as vertrekti
     for line in reader:
         delay = int(line[columns.index("delay")])
 
-        # if delay >= 60:
-            # gt += 1
+        if delay <= 30:
+            delays.append(delay)
 
-        if delay < 120:
-            delays.append(delay);
+scipy_params = st.expon.fit(delays, floc=0)
 
-plt.hist(delays, bins=30)
-plt.yscale('log', nonposy='clip')
+print(st.kstest(delays, 'expon', scipy_params)[1])
+x = np.linspace(1, 30, 100)
+plt.hist(delays, bins=15, label="Observations", density=True)
+plt.plot(x, st.expon(*scipy_params).pdf(x), label="scipy expon fit")
+plt.ylabel("Frequencies (normalized)")
+plt.xlabel("Minutes delay")
+plt.legend()
 plt.show()
 
-sns.distplot(delays)
-plt.yscale('log', nonposy='clip')
-plt.show()
-
-print(gt)
+# plt.plot(list(data.keys()), list(data.values()))
+# plt.show()
